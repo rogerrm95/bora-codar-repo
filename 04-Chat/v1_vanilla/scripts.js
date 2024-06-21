3afa8a2c8672c706e4ac0c78c8aa0f2b3c349824
Rogério Marques <rogerrm@live.com>
2024-06-20 22:57:01 -0300

    feat: Layout Chat (v.1-vanilha) done - #1

---
+++ 04-Chat/v1_vanilla/scripts.js
@@ -0,0 +1,7 @@
+const messageBox = document.getElementById('message-box')
+const messageInput = document.getElementById('message-input')
+
+messageBox.addEventListener('click', () => {
+    messageInput.focus()
+})
+    
