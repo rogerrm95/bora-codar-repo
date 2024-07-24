2447b4c61da77220aa3a571eded8ded6e7a30ca3
Rogério Marques <rogerrm@live.com>
2024-07-24 19:12:06 -0300

    feat: development layout (HTML/CSS) - Charts  #1

---
+++ 08-Dashboard/scripts.js
@@ -0,0 +1,11 @@
+// SCROLL REVEAL CONFIG //
+const scrollRevealConfig = {
+    interval: 100,
+    distance: '100%',
+    duration: 1000
+}
+
+// FAKE API //
+const api = axios.create({
+    baseURL: 'http://127.0.0.1:3004/'
+})
