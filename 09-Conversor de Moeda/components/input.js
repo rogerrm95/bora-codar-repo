8f05583cbd50346e4c11829d0a9657721c23bd64
Rogério Marques <rogerrm@live.com>
2024-08-01 23:30:06 -0300

    feat: create a input component and fetch API to calculate a exchange rate - #5

---
+++ 09-Conversor de Moeda/components/input.js
@@ -0,0 +1,37 @@
+// INPUTS //
+const inputOrigin = document.getElementById('currency-origin-input')
+const inputDestiny = document.getElementById('currency-destiny-input')
+
+const exchangeRateSpan = document.getElementById('exchange-rate-value')
+
+// FUNÇÃO - CARREGAR E CALCULAR A COTAÇAO //
+async function calculateExchangeRate(currencyFrom, currencyTo, event){   
+    if(currencyFrom.length > 0 && currencyTo.length > 0) {
+        const data = await api.get(`https://economia.awesomeapi.com.br/last/${currencyFrom}-${currencyTo}`).then(response => response.data)
+
+        const objectName = `${currencyFrom}${currencyTo}`
+        const exchangeRate = data[objectName].bid // TAXA DE COTAÇÃO
+
+        const valueConverted = (exchangeRate * event.target.value).toFixed(2)
+        inputDestiny.value = Number(valueConverted)
+
+        exchangeRateSpan.innerHTML = `Cotação: ${exchangeRate}`
+    }
+}
+
+function resetFields(){
+    inputOrigin.value = ''
+    inputDestiny.value = ''
+
+    document.getElementById('exchange-rate-value').innerHTML = ''
+}
+
+
+inputOrigin.addEventListener('blur', async (event) => {
+    const currencyOrigin = countryOriginSelect.getElementsByTagName('span')[0].textContent
+    const currencyDestiny = countryDestinySelect.getElementsByTagName('span')[0].textContent
+
+    await calculateExchangeRate(currencyOrigin, currencyDestiny, event)
+
+    event.stopPropagation()
+})
